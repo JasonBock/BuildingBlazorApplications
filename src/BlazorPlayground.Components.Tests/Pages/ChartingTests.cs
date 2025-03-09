99abaa3bc162e70b2fc835bbefb48a999a22e0ed
Jason Bock <jason.r.bock@outlook.com>
2025-03-09 16:12:26 -0500

    Got Rocks to work in test

--- src/BlazorPlayground.Components.Tests/Pages/ChartingTests.cs
+++ src/BlazorPlayground.Components.Tests/Pages/ChartingTests.cs
@@ -1,7 +1,11 @@
 ﻿using BlazorPlayground.Components.Pages;
 using Bunit;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
 using NUnit.Framework;
+using Rocks;
+using System.Collections.Immutable;
 
 namespace BlazorPlayground.Components.Tests.Pages;
 
@@ -16,7 +20,14 @@ public static class ChartingTests
 		var chartingInterop = context.JSInterop.SetupModule(Constants.ChartingFileLocation);
 		chartingInterop.Setup<object>(Constants.ChartingMethod, new InvocationMatcher(target =>
 		{
-			//Assert.That(target.Arguments[0] is ElementReference);
+			Assert.That(target.Arguments[0] is ElementReference);
+
+			var sequence = (ImmutableArray<int>)target.Arguments[1]!;
+			Assert.That(sequence, Is.EquivalentTo([5, 8, 4, 2, 1]));
+
+			var labels = (string[])target.Arguments[2]!;
+			Assert.That(labels, Is.EquivalentTo(["1", "2", "3", "4", "5"]));
+
 			return true;
 		}))
 		.SetResult(Task.CompletedTask);
@@ -28,16 +39,56 @@ public static class ChartingTests
 
 		await Assert.MultipleAsync(async () =>
 		{
-			charting.Instance.Start = "44";
+			charting.Instance.Start = "5";
 			await chartingButton.ClickAsync(new MouseEventArgs());
 			Assert.That(chartingCurrentInput.ToMarkup(),
-				Does.Contain("44,"));
+				Does.Contain("5, 8, 4, 2, 1"));
 		});
 	}
 
-	//[Test]
-	//public static async Task CreateSequenceWithMockAsync()
-	//{
+	[Test]
+	public static async Task CreateSequenceWithMockAsync()
+	{
+#pragma warning disable CA2012 // Use ValueTasks correctly
+		var objectReferenceExpectations = new IJSObjectReferenceCreateExpectations();
+		objectReferenceExpectations.Methods.InvokeAsync<object>(
+			Constants.ChartingMethod,
+			Arg.Validate<object?[]?>(values =>
+			{
+				Assert.That(values![0] is ElementReference);
 
-	//}
+				var sequence = (ImmutableArray<int>)values[1]!;
+				Assert.That(sequence, Is.EquivalentTo([5, 8, 4, 2, 1]));
+
+				var labels = (string[])values[2]!;
+				Assert.That(labels, Is.EquivalentTo(["1", "2", "3", "4", "5"]));
+
+				return true;
+			}))
+			.ReturnValue(ValueTask.FromResult<object>(new()));
+
+		var runtimeExpectations = new IJSRuntimeCreateExpectations();
+		runtimeExpectations.Methods.InvokeAsync<IJSObjectReference>(
+			 Constants.Import, new object?[] { Constants.ChartingFileLocation })
+			 .ReturnValue(ValueTask.FromResult(objectReferenceExpectations.Instance()));
+#pragma warning restore CA2012 // Use ValueTasks correctly
+
+		using var context = new BUnitTestContext();
+		context.Services.Add(new(typeof(IJSRuntime), runtimeExpectations.Instance()));
+		var charting = context.RenderComponent<Charting>();
+		var chartingButton = charting.Find("button");
+		var chartingInput = charting.Find("input");
+		var chartingCurrentInput = charting.Find("#currentSequence");
+
+		await Assert.MultipleAsync(async () =>
+		{
+			charting.Instance.Start = "5";
+			await chartingButton.ClickAsync(new MouseEventArgs());
+			Assert.That(chartingCurrentInput.ToMarkup(),
+				Does.Contain("5, 8, 4, 2, 1"));
+		});
+
+		objectReferenceExpectations.Verify();
+		runtimeExpectations.Verify();
+	}
 }
--- src/BlazorPlayground.Components.Tests/Shared.cs
+++ src/BlazorPlayground.Components.Tests/Shared.cs
@@ -1,4 +1,8 @@
 ﻿global using BUnitTestContext = Bunit.TestContext;
+using Microsoft.JSInterop;
 using NUnit.Framework;
+using Rocks;
 
 [assembly: Parallelizable(ParallelScope.Children)]
+[assembly: Rock(typeof(IJSObjectReference), BuildType.Create)]
+[assembly: Rock(typeof(IJSRuntime), BuildType.Create)]
