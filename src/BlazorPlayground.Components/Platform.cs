a9f55fecfd57a6eea69cfbbe9642402acfc6fb9a
Jason Bock <jason.r.bock@outlook.com>
2026-07-13 19:48:30 -0500

    Added C#-only component

---
+++ src/BlazorPlayground.Components/Platform.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System.Runtime.InteropServices;
+
+namespace BlazorPlayground.Components;
+
+public sealed class Platform
+	: ComponentBase
+{
+	protected override void BuildRenderTree(RenderTreeBuilder builder)
+	{
+		ArgumentNullException.ThrowIfNull(builder);
+		builder.OpenElement(0, "span");
+		builder.AddContent(1, RuntimeInformation.OSDescription);
+		builder.CloseElement();
+	}
+}
